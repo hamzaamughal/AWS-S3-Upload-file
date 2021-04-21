9ac7ccfa67d16b9002b1dae50cc69912e96d1929
hamzamughal <mughalh473@gmail.com>
2021-04-21 19:34:51 +0500

    s3 setup

--- index.js
+++ index.js
@@ -8,10 +8,30 @@ const uuid = require('uuid').v4;
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+const storage = multer.memoryStorage({
+    destination: function (req, file, callback) {
+        callback(null, "")
+    }
+})
+
+const s3 = new AWS.S3()
+//     {
+//     credentials: {
+//         accessKeyId: process.env.AWS_ACCESS_KEY,
+//         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+//     }
+// }
+
+const upload = multer({ storage }).single('image')
 
 app.get("/", (req, res) => {
     res.render("index")
+})
+
+app.post('/upload', (req, res) => {
+
 })
 
 app.listen(port, () => {
