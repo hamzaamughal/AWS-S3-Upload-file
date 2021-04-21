36c2094e4426ab6d1376596a820991859a5c220c
hamzamughal <mughalh473@gmail.com>
2021-04-21 15:48:48 +0500

    added ENV file and setup

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+const multer = require('multer');
+const AWS = require('aws-sdk')
+const uuid = require('uuid').v4;
+
+
+const app = express();
+const port = process.env.PORT || 3000;
